137e43b8518364a1baa29365d4438aeff0f4a733
Michael Plomer <michael.plomer@iter8.de>
2023-10-05 07:25:44 -0700

    Addes listrequest

--- src/main/java/de/iter8/google/api/webclient/ListRequest.java
+++ src/main/java/de/iter8/google/api/webclient/ListRequest.java
@@ -1,2 +1,15 @@
-package de.iter8.google.api.webclient;public interface ListRequest {
+package de.iter8.google.api.webclient;
+
+import reactor.core.publisher.Mono;
+
+public interface ListRequest<I, P extends ListResultPage<I>, ME extends ListRequest<I, P, ME>> {
+    ME pageToken(String pageToken);
+
+    default ME syncToken(String syncToken) {
+        return (ME) this;
+    }
+
+    ME maxResults(Integer maxResults);
+
+    Mono<P> execute();
 }
--- src/main/java/de/iter8/google/api/webclient/ListResultPage.java
+++ src/main/java/de/iter8/google/api/webclient/ListResultPage.java
@@ -1,2 +1,12 @@
-package de.iter8.google.api.webclient;public interface ListResultPage {
+package de.iter8.google.api.webclient;
+
+import java.util.List;
+
+public interface ListResultPage<I> {
+    String getNextPageToken();
+    String getNextSyncToken();
+    List<I> getItems();
+    String getKind();
+    String getEtag();
+
 }
